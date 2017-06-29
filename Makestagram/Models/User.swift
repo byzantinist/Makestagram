70bbd41e4a7857a2ca0b682b543d64be9216dca4
Eric Chiang <echiang777@yahoo.com>
2017-06-29 13:03:41 -0500

    Keep User Logged In with User Defaults
    Complete Section 12.

--- Makestagram/Models/User.swift
+++ Makestagram/Models/User.swift
@@ -7,9 +7,10 @@
 //
 
 import Foundation
+import UIKit
 import FirebaseDatabase.FIRDataSnapshot
 
-class User {
+class User: NSObject {
     
     // MARK: - Properties
     
@@ -21,6 +22,8 @@ class User {
     init(uid: String, username: String) {
         self.uid = uid
         self.username = username
+        
+        super.init()
     }
     
     init?(snapshot: DataSnapshot) {
@@ -30,8 +33,21 @@ class User {
             
         self.uid = snapshot.key
         self.username = username
+        
+        super.init()
     }
 
+    required init?(coder aDecoder: NSCoder) {
+        guard let uid = aDecoder.decodeObject(forKey: Constants.UserDefaults.uid) as? String,
+            let username = aDecoder.decodeObject(forKey: Constants.UserDefaults.username) as? String
+            else { return nil }
+        
+        self.uid = uid
+        self.username = username
+        
+        super.init()
+    }
+    
     // MARK: - Singleton
     
     private static var _current: User?
@@ -49,10 +65,25 @@ class User {
     static func setCurrent(_ user: User) {
         _current = user
     }
+    
+    class func setCurrent(_ user: User, writeToUserDefaults: Bool = false) {
+        if writeToUserDefaults {
+            let data = NSKeyedArchiver.archivedData(withRootObject: user)
+            
+            UserDefaults.standard.set(data, forKey: Constants.UserDefaults.currentUser)
+        }
+        
+        _current = user
+    }
 
 }
 
-
+extension User: NSCoding {
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(uid, forKey: Constants.UserDefaults.uid)
+        aCoder.encode(username, forKey: Constants.UserDefaults.username)
+    }
+}
 
 
 
