04cdaa3e33e7003bc7e693e339e075bf60cb82e3
Eric Chiang <echiang777@yahoo.com>
2017-06-30 10:39:33 -0500

    Complete Taking Photos
    Finished with Section 13.

--- Makestagram/Helpers/MGPhotoHelper.swift
+++ Makestagram/Helpers/MGPhotoHelper.swift
@@ -20,16 +20,16 @@ class MGPhotoHelper: NSObject {
         let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .actionSheet)
         
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { action in
-                // do nothing yet...
+            let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { [unowned self] action in
+                    self.presentImagePickerController(with: .camera, from: viewController)
             })
             
             alertController.addAction(capturePhotoAction)
         }
         
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { action in
-                // do nothing yet...
+            let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { [unowned self] action in
+                    self.presentImagePickerController(with: .photoLibrary, from: viewController)
             })
             
             alertController.addAction(uploadAction)
@@ -41,4 +41,26 @@ class MGPhotoHelper: NSObject {
         viewController.present(alertController, animated: true)
     }
     
+    func presentImagePickerController(with sourceType: UIImagePickerControllerSourceType, from viewController: UIViewController) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = sourceType
+        imagePickerController.delegate = self
+        
+        viewController.present(imagePickerController, animated: true)
+    }
+    
+}
+
+extension MGPhotoHelper: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            completionHandler?(selectedImage)
+        }
+        
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
 }
